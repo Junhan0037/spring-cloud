1255a15e53046f9190f192effbaf6ee847a6a23e
Junhan0037 <wnsgks0037@naver.com>
2021-08-02 12:57:47 +0900

    feat: Filter 적용
    - Java
    - yml

--- first-service/src/main/java/com/firstservice/controller/WelcomeController.java
+++ first-service/src/main/java/com/firstservice/controller/WelcomeController.java
@@ -2,6 +2,7 @@ package com.firstservice.controller;
 
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestHeader;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -13,6 +14,12 @@ public class WelcomeController {
     @GetMapping("/welcome")
     public String welcome() {
         return "Welcome to the First service.";
+    }
+
+    @GetMapping("/message")
+    public String message(@RequestHeader("first-request") String header) {
+        log.info(header);
+        return "Hello World in First Service.";
     }
 
 }
--- second-service/src/main/java/com/secondservice/controller/WelcomeController.java
+++ second-service/src/main/java/com/secondservice/controller/WelcomeController.java
@@ -2,6 +2,7 @@ package com.secondservice.controller;
 
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestHeader;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -13,6 +14,12 @@ public class WelcomeController {
     @GetMapping("/welcome")
     public String welcome() {
         return "Welcome to the Second service.";
+    }
+
+    @GetMapping("/message")
+    public String message(@RequestHeader("second-request") String header) {
+        log.info(header);
+        return "Hello World in Second Service.";
     }
 
 }
