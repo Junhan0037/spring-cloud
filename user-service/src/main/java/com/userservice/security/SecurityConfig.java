686e5a5674660dde2616dc5935d58672a76457a4
Junhan0037 <wnsgks0037@naver.com>
2021-08-20 13:25:22 +0900

    feat: Spring Cloud Config 연동
    - User Microservice

--- user-service/src/main/java/com/userservice/security/SecurityConfig.java
+++ user-service/src/main/java/com/userservice/security/SecurityConfig.java
@@ -22,6 +22,7 @@ public class SecurityConfig extends WebSecurityConfigurerAdapter {
     @Override
     protected void configure(HttpSecurity http) throws Exception {
         http.authorizeRequests()
+                .antMatchers("/actuator/**").permitAll()
                 .antMatchers("/**").permitAll()
                 .and()
                 .addFilter(getAuthenticationFilter());
